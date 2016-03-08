3ba4f3d762797fb038ae91a008bae4a0130fd3e7
Aayush Goyal <aayushgoyal1994@gmail.com>
2016-03-08 12:49:08 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,39 @@
+<html lan="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Dengue</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<!--nav-bar-start-->
+<nav class="navbar navbar-inverse navbar-fixed-top no-margin">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" 
+                  aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+           <ul class="nav navbar-nav">
+              <li ><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
+              <li class="active"><a href="about.php">About Us</a></li>
+           </ul>
+           </div>
+  </div>
+</nav>
+<!--nav-bar-ends-->
+<div id="about">
+<p>
+ This website basically allow the doctors and hospitals all over the world to submit the information about their Dengue patients. This information is showed on heat maps according to number of patients belonging to particular area. This information can be useful to NGO's, health agencies and governments to keep track of this diseases in their areas.
+Also various filters are provided for better understanding of the information.
+</p>
+</div>
+</body>
+</html>
